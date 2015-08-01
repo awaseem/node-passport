42b98938cb50c6efc93801e8aa80dc0a9b9b3545
Ali Waseem <waseema393@gmail.com>
2015-07-31 19:14:42 -0600

    added facebook auth to routes and passport

--- app/routes.js
+++ app/routes.js
@@ -35,6 +35,15 @@ module.exports = function(app, passport) {
         });
     });
 
+    app.get("/auth/facebook", passport.authenticate("facebook", {
+        scope: "email"
+    }));
+
+    app.get("/auth/facebook/callback", passport.authenticate("facebook", {
+        successRedirect: "/profile",
+        failureRedirect: "/"
+    }));
+
     app.get('/logout', function(req, res) {
         req.logout();
         res.redirect('/');
--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,9 @@
 // config/passport.js
 
 var LocalStrategy = require('passport-local').Strategy;
+var FacebookStrategy = require("passport-facebook").Strategy;
+
+var configAuth = require("./auth");
 
 var User = require('../app/models/users');
 
@@ -16,6 +19,7 @@ module.exports = function(passport) {
         });
     });
 
+    // Local sign up
     passport.use('local-signup', new LocalStrategy({
             usernameField : 'email',
             passwordField : 'password',
@@ -71,4 +75,40 @@ module.exports = function(passport) {
             });
 
         }));
+
+    // Facebook login
+    passport.use(new FacebookStrategy({
+        clientID: configAuth.facebookAuth.clientID,
+        clientSecret: configAuth.facebookAuth.clientSecret,
+        callbackURL: configAuth.facebookAuth.callbackURL,
+        profileFields: ["displayName", "emails"]
+    }, function (token, refreshToken, profile, done) {
+        process.nextTick(function () {
+            User.findOne({ 'facebook.id': profile.id }, function (err, user) {
+                if (err) {
+                    return done(err)
+                }
+
+                if (user) {
+                    return done(null, user);
+                }
+                else {
+                    var newUser = new User();
+
+                    // set all of the facebook information in our user model
+                    newUser.facebook.id = profile.id;
+                    newUser.facebook.token = token;
+                    newUser.facebook.name = profile.displayName;
+                    newUser.facebook.email = profile.emails[0].value;
+
+                    newUser.save(function (err) {
+                        if (err) {
+                            throw err;
+                        }
+                        return done(null, newUser);
+                    });
+                }
+            });
+        });
+    }));
 };
